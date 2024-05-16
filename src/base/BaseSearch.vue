16923579f5fb5569aa62d81f626476681647fc01
KULINININ <aleks680026032001@gmail.com>
2024-05-16 14:59:32 +0500

    feat(BaseSearch): use v-model

--- src/base/BaseSearch.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <input
-    class="search-card__input border rounded-lg p-3 w-full"
-    type="text"
-    v-model="searchQuery"
-    placeholder="Введите id или имя"
-  />
-</template>
---
+++ src/components/Base/BaseSearch.vue
@@ -0,0 +1,20 @@
+<template>
+  <input
+    class="search-card__input border rounded-lg p-3 w-full"
+    type="text"
+    :value="props.value"
+    @input="emit('update:value', $event.target.value)"
+    placeholder="Введите id или username"
+  />
+</template>
+
+<script setup lang="ts">
+const props = defineProps({
+  value: {
+    type: String,
+    required: true
+  }
+})
+
+const emit = defineEmits(['update:value'])
+</script>
