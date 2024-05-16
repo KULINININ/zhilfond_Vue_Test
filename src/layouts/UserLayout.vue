eba25921e4f4ef056099d5d9d63b49e94450cede
KULINININ <aleks680026032001@gmail.com>
2024-05-16 14:58:32 +0500

    chore(UserLayout): add font-bold

--- src/layouts/UserLayout.vue
+++ src/layouts/UserLayout.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="user-layout container mx-auto my-10 space-y-10">
     <div class="user-layout__header flex inline justify-between">
-      <div class="user-layout__header-logo text-3xl text-[#E31F24]">Жилфонд</div>
+      <div class="user-layout__header-logo font-bold text-3xl text-[#E31F24]">Жилфонд</div>
       <div class="user-layout__header-user text-[#6d6d6d]">Пользователь</div>
     </div>
     <div class="user-layout__content">
