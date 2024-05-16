2b61d725b51d01811d3ac82b0cb08672bb470e2e
KULINININ <aleks680026032001@gmail.com>
2024-05-16 15:01:25 +0500

    feat(UserEmploeesView): add base logic

--- src/views/UserEmploeesView.vue
+++ src/views/UserEmploeesView.vue
@@ -1,15 +1,39 @@
 <template>
   <div class="employees-view grid grid-cols-3 rounded-lg shadow-[0px_0px_12px_0px_rgba(0,0,0,0.1)]">
-    <div class="employee-view__search col-span-1 p-4 border-r-2 border-gray-200">
+    <div class="employee-view__search col-span-1 p-4 border-r-2 border-gray-200 bg-[#fdfdfd]">
       <SearchCard />
     </div>
     <div class="employee-view__info col-span-2 p-4">
-      <UserCard />
+      <div class="employee-view__info__card" v-if="selectedUser && !userDataLoading">
+        <UserCardFull :user="selectedUser" />
+      </div>
+      <div
+        class="employee-view__info__card__loading-skeleton flex flex-col items-center relative"
+        v-else-if="userDataLoading"
+      >
+        <UserCardFullSkeleton />
+        <div class="employee-view__info__card__loading-loader absolute top-1/2 left-1/2 z-10">
+          <LoadingIcon />
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import UserCard from '../components/UserCardFull.vue'
+import { useStore } from 'vuex'
+import { computed } from 'vue'
+
+import UserCardFull from '../components/UserCardFull.vue'
+import UserCardFullSkeleton from '../components/Skeletons/UserCardFullSkeleton.vue'
+import LoadingIcon from '../assets/icons/LoadingIcon.vue'
 import SearchCard from '../components/SearchCard.vue'
+
+const store = useStore()
+
+const getUsers = () => store.dispatch('getUsers')
+getUsers()
+
+const selectedUser = computed(() => store.getters.selectedUser)
+const userDataLoading = computed(() => store.getters.userDataLoading)
 </script>
