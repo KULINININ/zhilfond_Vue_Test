495d55b0f37e7e2381334135ddd260eb8da41b51
KULINININ <aleks680026032001@gmail.com>
2024-05-16 14:55:22 +0500

    feat(types): add user type

---
+++ src/types/user.ts
@@ -0,0 +1,23 @@
+import { Company } from '@types/company.ts'
+
+interface Address {
+  readonly city: string
+  readonly geo: {
+    lat: number
+    lng: number
+  }
+  readonly street: string
+  readonly suite: string
+  readonly zipcode: string
+}
+
+export interface User {
+  readonly id: number
+  readonly address: Address
+  readonly company: Company
+  readonly email: string
+  readonly name: string
+  readonly phone: string
+  readonly username: string
+  readonly website: string
+}
