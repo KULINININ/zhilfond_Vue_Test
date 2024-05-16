e73f1350997b4f05d9ef4d663ed6155c4bcb4a7f
KULINININ <aleks680026032001@gmail.com>
2024-05-16 15:01:10 +0500

    feat(SearchCard): add base logic

--- src/components/SearchCard.vue
+++ src/components/SearchCard.vue
@@ -1,13 +1,70 @@
 <template>
-  <div class="search-card bg-[#fdfdfd] space-y-4">
+  <div class="search-card space-y-4">
     <div class="search-card__header font-bold text-[#333333]">Поиск сотрудника</div>
-    <BaseSearch />
+    <BaseSearch v-model:value="search" />
+    <div>{{ search }}</div>
     <div class="search-card__result font-bold text-[#333333]">Результаты</div>
-    <UserCardMini v-for="id in 3" :key="id" />
+    <div class="search-card__users space-y-4" v-if="!allUsersLoading">
+      <UserCardMini
+        v-for="user in users"
+        :key="user.id"
+        :name="user.name"
+        :email="user.email"
+        :active="user.id === selectedUser?.id"
+        @click="doSelectUser(user)"
+      />
+    </div>
+    <div
+      class="search-card__users__loading-skeleton space-y-4 flex flex-col items-center relative"
+      v-for="i in 3"
+      :key="i"
+      v-else
+    >
+      <UserCardMiniSkeleton />
+      <div class="search-card__users__loading-loader absolute top-1/4 left-1/2 z-10">
+        <LoadingIcon />
+      </div>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import UserCardMini from './UserCardMini.vue'
-import BaseSearch from '../base/BaseSearch.vue'
+import UserCardMiniSkeleton from './Skeletons/UserCardMiniSkeleton.vue'
+import BaseSearch from '../components/Base/BaseSearch.vue'
+import LoadingIcon from '../assets/icons/LoadingIcon.vue'
+
+import { useStore } from 'vuex'
+import { computed, ref } from 'vue'
+import { debounce } from 'lodash'
+
+import type { User } from '@types/user.ts'
+
+// const debouncedUsersFilter = debounce((searchString) => {
+//   const params = searchString.split(',')
+//   console.log('params', params)
+//   // store.dispatch('getUsersByParams', { name: filterParams })
+//   // console.log('filterParams', { name: filterParams })
+// }, 500)
+
+const store = useStore()
+
+const search = ref('')
+
+const users = computed<User[]>(() => {
+  const searchString = search.value.toLowerCase()
+  return store.getters.users.filter((user: User) => {
+    return (
+      user.name.toLowerCase().includes(searchString) ||
+      user.email.toLowerCase().includes(searchString)
+    )
+  })
+})
+
+const selectedUser = computed(() => store.getters.selectedUser)
+const allUsersLoading = computed(() => store.getters.allUsersLoading)
+
+const doSelectUser = (user) => {
+  store.dispatch('getUsersById', { id: user.id })
+}
 </script>
