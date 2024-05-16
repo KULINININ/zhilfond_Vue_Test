c7373d4e9097479007e2bed402045ceb3826818a
KULINININ <aleks680026032001@gmail.com>
2024-05-16 15:00:28 +0500

    feat(UserCardMini): add props

--- src/components/UserCardMini.vue
+++ src/components/UserCardMini.vue
@@ -1,11 +1,31 @@
 <template>
-  <div class="user-card-mini grid grid-cols-3 rounded-lg shadow-[0px_0px_6px_0px_rgba(0,0,0,0.1)]">
+  <div
+    class="user-card-mini grid grid-cols-3 rounded-lg shadow-[0px_0px_6px_0px_rgba(0,0,0,0.1)] cursor-pointer"
+    :class="active ? 'bg-[#f5f5f5]' : ''"
+  >
     <div class="user-card-mini__image col-span-1 p-4 border-r-2 border-gray-200">
-      <img src="" alt="user" />
+      <img src="https://via.placeholder.com/200/92c952" alt="user" />
     </div>
     <div class="user-card-mini__info col-span-2 p-4">
-      <div class="user-card-mini__name font-bold">Bret</div>
-      <div class="user-card-email text-gray-500">Sincere@april.biz</div>
+      <div class="user-card-mini__name font-bold">{{ name }}</div>
+      <div class="user-card-email text-gray-500">{{ email }}</div>
     </div>
   </div>
 </template>
+
+<script setup lang="ts">
+const props = defineProps({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  active: {
+    type: Boolean,
+    required: true
+  }
+})
+</script>
