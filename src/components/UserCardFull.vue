dd093d27aabad6d2fa700c5822806a549a400fe5
KULINININ <aleks680026032001@gmail.com>
2024-05-16 15:00:20 +0500

    feat(UserCardFull): add props

--- src/components/UserCardFull.vue
+++ src/components/UserCardFull.vue
@@ -2,24 +2,28 @@
   <!-- <div class="user-card">
     <div class="font-bold text-[#333333]">Выберите сотрудника, чтобы посмотреть его профиль</div>
   </div> -->
-  <div class="user-card grid grid-cols-2">
+  <div class="user-card grid grid-cols-2" v-if="user">
     <div class="user-card__image col-span-1 p-4">
-      <img src="" alt="user" />
+      <img src="https://via.placeholder.com/600/92c952" alt="user" />
     </div>
     <div class="user-card__info col-span-1 p-4 space-y-4">
       <div class="user-card__info-main">
-        <div class="user-card__info-main__name font-bold">Bret</div>
-        <div class="user-card__info-main__email text-gray-500">Sincere@april.biz</div>
-        <div class="user-card__info-main__phone text-gray-500">010-692-6593 x09125</div>
+        <div class="user-card__info-main__name font-bold">{{ user.name }}</div>
+        <div class="user-card__info-main__email text-gray-500">{{ user.email }}</div>
+        <div class="user-card__info-main__phone text-gray-500">{{ user.phone }}</div>
       </div>
       <div class="user-card__info-about__title text-2xl">О себе</div>
       <div class="user-card__info-about__text text-gray-500">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-        labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-        laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
-        voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-        cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        {{ user }}
       </div>
     </div>
   </div>
 </template>
+
+<script setup lang="ts">
+const props = defineProps({
+  user: {
+    type: Object
+  }
+})
+</script>
