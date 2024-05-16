b012c870d8a9f44f2f752bddd118b3ae096240ba
KULINININ <aleks680026032001@gmail.com>
2024-05-16 14:56:35 +0500

    feat(skeletons): add userCardFullSkeleton

---
+++ src/components/Skeletons/UserCardFullSkeleton.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="user-card grid grid-cols-2 animate-pulse">
+    <div class="user-card__image col-span-1 p-4">
+      <img src="https://via.placeholder.com/600/92c952" alt="user" />
+    </div>
+    <div class="user-card__info col-span-1 p-4 space-y-4">
+      <div class="user-card__info-main">
+        <div class="user-card__info-main__name bg-gray-200">&nbsp;</div>
+        <div class="user-card__info-main__email bg-gray-200">&nbsp;</div>
+        <div class="user-card__info-main__phone bg-gray-200">&nbsp;</div>
+      </div>
+      <div class="user-card__info-about__title text-2xl">О себе</div>
+      <div class="user-card__info-about__text bg-gray-200">&nbsp;</div>
+    </div>
+  </div>
+</template>
