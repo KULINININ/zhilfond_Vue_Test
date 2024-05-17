9a44870abb57fe29d4e62f10059a2adaa00bb8b6
KULINININ <aleks680026032001@gmail.com>
2024-05-17 08:53:11 +0500

    feat: add aliases

--- vite.config.ts
+++ vite.config.ts
@@ -9,11 +9,11 @@ export default defineConfig({
   plugins: [vue(), VueDevTools()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-      // '@layouts': fileURLToPath(new URL('./src/layouts', import.meta.url)),
-      // '@store': fileURLToPath(new URL('./src/store', import.meta.url)),
-      // '@types': fileURLToPath(new URL('./src/types', import.meta.url)),
-      // '@components': fileURLToPath(new URL('./src/components', import.meta.url))
+      '@/': fileURLToPath(new URL('./src/', import.meta.url)),
+      '@layouts/': fileURLToPath(new URL('./src/layouts/', import.meta.url)),
+      '@store/': fileURLToPath(new URL('./src/store/', import.meta.url)),
+      '@types/': fileURLToPath(new URL('./src/types/', import.meta.url)),
+      '@components/': fileURLToPath(new URL('./src/components/', import.meta.url))
     }
   }
 })
