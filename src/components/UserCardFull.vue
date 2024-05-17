86fc61d2965c239dc8646167dcb1d19fcb203904
KULINININ <aleks680026032001@gmail.com>
2024-05-17 08:13:10 +0500

    chore(UserCardFull): add text for params

--- src/components/UserCardFull.vue
+++ src/components/UserCardFull.vue
@@ -7,12 +7,18 @@
       <img src="https://via.placeholder.com/600/92c952" alt="user" />
     </div>
     <div class="user-card__info col-span-1 p-4 space-y-4">
-      <div class="user-card__info-main">
+      <div class="user-card__info-main space-y-2">
         <div class="user-card__info-main__name font-bold">{{ user.name }}</div>
-        <div class="user-card__info-main__email text-gray-500">{{ user.email }}</div>
-        <div class="user-card__info-main__phone text-gray-500">{{ user.phone }}</div>
+        <div class="user-card__info-main__email">
+          <span class="font-bold">email: </span>
+          <span class="text-gray-500">{{ user.email }}</span>
+        </div>
+        <div class="user-card__info-main__phone">
+          <span class="font-bold">phone: </span>
+          <span class="text-gray-500">{{ user.phone }}</span>
+        </div>
       </div>
-      <div class="user-card__info-about__title text-2xl">О себе</div>
+      <div class="user-card__info-about__title text-2xl">О себе:</div>
       <div class="user-card__info-about__text text-gray-500">
         {{ user }}
       </div>
