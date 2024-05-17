4e9897cd32ef7f7a23ccb76087159dd329c90c55
KULINININ <aleks680026032001@gmail.com>
2024-05-17 09:24:51 +0500

    test(Error): add throw error for tests

--- src/api/api.ts
+++ src/api/api.ts
@@ -8,6 +8,8 @@ export async function fetchUsersByParams(
   params: { ids: string; usernames: string },
   page: number
 ): Promise<any> {
+  // throw new Error('Invalid page number. Page number must be greater than or equal to 1.')
+
   const response = await fetch(
     `https://jsonplaceholder.typicode.com/users?_page=${page}&_limit=3&${params.ids}`
   )
@@ -16,6 +18,7 @@ export async function fetchUsersByParams(
 }
 
 export async function fetchUsersById(id: number): Promise<any> {
+  // throw new Error('Invalid page number. Page number must be greater than or equal to 1.')
   const response = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
   const data = await response.json()
   return data
